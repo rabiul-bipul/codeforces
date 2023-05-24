168c70b6b5d82210af55be68c5d2de54354b9c92
Rabiul Islam Bipul <132159770+rabiul-bipul@users.noreply.github.com>
2023-05-24 21:35:16 +0600

    Create 4A - Watermelon

---
+++ 4A - Watermelon.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int w;
+    scanf("%d",&w);
+
+    if(w>2)
+    {
+        if(w%2==0)
+
+        {
+            printf("YES");
+        }
+        else
+        {
+            printf("NO");
+        }
+    }
+    else
+    {
+        printf("NO");
+    }
+}
