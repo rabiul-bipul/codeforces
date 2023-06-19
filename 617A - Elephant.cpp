573f4bb4faf48d02fc851cb4bd1070dff8b19303
Rabiul Islam Bipul <132159770+rabiul-bipul@users.noreply.github.com>
2023-06-20 00:52:39 +0600

    Create 617A - Elephant

---
+++ 617A - Elephant.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long x,a;
+    cin>>x;
+    if(x%5==0)
+    {
+        a=x/5;
+    }
+    else
+    {
+        a=(x/5)+1;
+    }
+cout<<a;
+
+
+
+}
