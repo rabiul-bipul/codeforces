470484d9c1f905cf344aa5298584840ae6b53ec9
Rabiul Islam Bipul <132159770+rabiul-bipul@users.noreply.github.com>
2023-07-18 22:00:06 +0600

    Create 96A - Football

---
+++ 96A - Football.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    int a=0,b=0,c=0;
+    cin>>s;
+
+    for(int i=0; i<s.size(); i++)
+    {
+        if(s[i]=='1')
+        {
+            a++;
+            b=0;
+        }
+       else
+        {
+            b++;
+            a=0;
+        }
+        if(a==7||b==7)
+        {
+            c=1;
+        }
+    }
+
+    if(c==1)
+    {
+        cout<<"YES";
+    }
+    else
+    {
+        cout<<"NO";
+    }
+}
