698f74f903e035b7bff90449ddcdd511507aabc5
Rabiul Islam Bipul <132159770+rabiul-bipul@users.noreply.github.com>
2023-08-01 17:14:39 +0600

    Create 61A - Ultra-Fast Mathematician.cpp

---
+++ 61A - Ultra-Fast Mathematician.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   string a,b;
+
+   cin>>a>>b;
+
+   for(int i=0;i<a.size();i++)
+   {
+      if((a[i]=='0'&&b[i]=='0')||(a[i]=='1' && b[i]=='1'))
+      {
+         cout<<"0";
+      }else
+      {
+         cout<<"1";
+      }
+   }
+
+
+
+}
